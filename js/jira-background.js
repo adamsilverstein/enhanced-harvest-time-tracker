fe16347660731a51c6ca6935140eb42859de942f
Ivan Kruchkoff <ivank@10up.com>
2014-10-21 21:45:56 +0200

    VOL-171 Allow for multiple browse tabs

--- js/jira-background.js
+++ js/jira-background.js
@@ -1,16 +1,17 @@
-var ticket = null;
+var tickets = {};
 
 // Receive ticket details from js/profiles/jira.js
 // Send them to js/profiles/jira-harvest.js when requested.
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
+        var key = sender.tab.id;
         // Receiving from the jira page, we set the ticket.
         if (request.isTicket && typeof request.ticket === "object" && typeof request.ticket.id === "string") {
-            ticket = request.ticket;
-        } else if (typeof ticket === "object") {
+            tickets[key] = request.ticket;
+        } else if (typeof tickets[key] === "object") {
             // Receiving from the harvest page, we already have the ticket, so we respond with it
-            sendResponse(ticket);
+            sendResponse(tickets[key]);
         }
     }
 );
