3f7df86b07e972be89f5793c0d1e576e1a902b13
Ivan Kruchkoff <ivank@10up.com>
2014-10-24 17:40:39 +0200

    Select KO automatically and autofill search with Development

--- js/profiles/jira-harvest.js
+++ js/profiles/jira-harvest.js
@@ -21,6 +21,24 @@
 
                 // Set the note field of the harvest iframe to contain our ticket details
                 $('#notes').text(note);
+                if (response.hash_parameters.note) {
+
+                    $('option').removeAttr('selected');
+                    // Note is added, we came from helpscout. Select the KO items.
+                    $('option:contains(CocaCola Support)').attr('selected', 'selected');
+                    $('.chzn-single span:first').text("CocaCola Support");
+                    $('#task_id_chzn').trigger('mousedown');
+                    $('.chzn-single span:last').text("Development");
+                    $('.chzn-container:last').addClass('chzn-with-drop');
+                    $('.chzn-search input:last').val('Development');
+                    $('.chzn-search input:last').trigger('focus');
+                    $('.chzn-search input:last').trigger('mousedown');
+                    window.setTimeout(function() { 
+                        // $('.chzn-container:last').removeClass('chzn-with-drop');
+                        // This works if you open the iframe in a new tab to trigger the dropdown from where we can select the Development item, but not when the iframe is loaded in the JIRA page. Someone smarter than me have any suggestions?
+                        $('#task_id_chzn').trigger('mousedown');
+                    }, 500);
+                }
 
             }
         }
